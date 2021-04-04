a1029f823d6b6ccfbd213418eee76b328c5a8f53
tmxSjvX9D4Nqtcs <spend.a.penny@hotmail.com>
2021-04-04 23:19:04 +0100

    Update calendar logic

---
+++ script.js
@@ -0,0 +1,78 @@
+const date = new Date();
+
+const renderCalendar = () => {
+    
+const monthDays = document.querySelector('.days');
+
+const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
+const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+
+const convertDays = (day) => {
+    if(day === 0){
+        return 6;
+    }
+    else{
+        return day - 1;
+    }
+}
+
+const firstDayIndex = convertDays(new Date(date.getFullYear(), date.getMonth(), 1).getDay());
+
+const lastDayIndex = convertDays(new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay());
+
+const nextDays = 7 - lastDayIndex -1;
+
+console.log(firstDayIndex);
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+document.querySelector('.date h1').innerHTML = months[date.getMonth()];
+
+document.querySelector('.date p').innerHTML = new Date().toDateString();
+
+
+let days = "";
+
+for(let x = firstDayIndex; x > 0; x--){
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`
+}
+
+for(let i = 1; i <= lastDay; i++){
+    if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()){
+        days += `<div class="today">${i}</div>`
+    }else{
+        days += `<div>${i}</div>`  
+    }
+}
+
+for(let i = 1; i <= nextDays; i++){
+    days += `<div class="next-date">${i}</div>`
+}
+monthDays.innerHTML = days;
+}
+
+document.querySelector('.prev').addEventListener('click', () => {
+    date.setMonth(date.getMonth()-1);
+    renderCalendar();
+});
+
+document.querySelector('.next').addEventListener('click', () =>{
+    date.setMonth(date.getMonth()+1);
+    renderCalendar();
+});
+
+renderCalendar();
