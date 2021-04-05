0ea14a7d23f96222002a8b428c696f8bc3faebe1
tmxSjvX9D4Nqtcs <spend.a.penny@hotmail.com>
2021-04-05 21:08:22 +0100

    Update to calendar, to allow selection of days in different months

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const date = new Date();
+const selectedDate = new Date();
 
 const renderCalendar = () => {
     
@@ -19,12 +20,6 @@ const convertDays = (day) => {
 
 const firstDayIndex = convertDays(new Date(date.getFullYear(), date.getMonth(), 1).getDay());
 
-const lastDayIndex = convertDays(new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay());
-
-const nextDays = 7 - lastDayIndex -1;
-
-console.log(firstDayIndex);
-
 const months = [
     "January",
     "February",
@@ -42,27 +37,54 @@ const months = [
 
 document.querySelector('.date h1').innerHTML = months[date.getMonth()];
 
-document.querySelector('.date p').innerHTML = new Date().toDateString();
+document.querySelector('.date p').innerHTML = selectedDate.toDateString();
 
 
 let days = "";
+let totalDays = 0;
 
 for(let x = firstDayIndex; x > 0; x--){
     days += `<div class="prev-date">${prevLastDay - x + 1}</div>`
+    totalDays += 1;
 }
 
 for(let i = 1; i <= lastDay; i++){
-    if(i === new Date().getDate() && date.getMonth() === new Date().getMonth()){
-        days += `<div class="today">${i}</div>`
-    }else{
-        days += `<div>${i}</div>`  
+    if(i === new Date().getDate() && date.getMonth() === new Date().getMonth() && date.getFullYear() === new Date().getFullYear()){
+
+        if(date.toString() === selectedDate.toString()){
+            days += `<div class="selectedToday">${i}</div>`
+        }
+        else{
+            days += `<div today class="today">${i}</div>`
+        }
+        totalDays += 1;
+    }
+    else if(i === selectedDate.getDate() && date.getMonth() === selectedDate.getMonth() && date.getFullYear() === selectedDate.getFullYear()){
+        days += `<div selectedDay class="selectedDay">${i}</div>`
+        totalDays += 1;
+    }
+    else{
+        days += `<div day>${i}</div>`  
+        totalDays += 1;
     }
 }
 
-for(let i = 1; i <= nextDays; i++){
+for(let i = 1; totalDays < 42; i++){
     days += `<div class="next-date">${i}</div>`
+    totalDays += 1;
 }
 monthDays.innerHTML = days;
+
+document.querySelectorAll('[day],[today]').forEach(day => {
+    day.addEventListener('click', () => {
+        selectedDate.setMonth(date.getMonth());
+        selectedDate.setFullYear(date.getFullYear());
+        selectedDate.setDate(day.innerHTML);
+        renderCalendar();
+        console.log(selectedDate);
+    })
+})
+
 }
 
 document.querySelector('.prev').addEventListener('click', () => {
